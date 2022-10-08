da5e4d947a100e43bee027fe6f47c3c5289c1799
bdsalako@gmail.com <bdsalako@gmail.com>
2022-10-07 20:00:38 -0400

    2

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,9 +3,19 @@ import{ Component } from '@angular/core'
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    
+    styleUrls: ['./server.component.css']
 
 })
 export class ServerComponent {
+    username: string = "David Salako";
+
+    Constructor(){
+        this.username = "David Salako";
+    }
+    Updater(){
+        this.username ="";
+        return "Temitope Salako";
+    
+    }
 
 }
