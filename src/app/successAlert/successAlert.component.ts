1801483930f517d6ed41d924e7441e91790e327a
bdsalako@gmail.com <bdsalako@gmail.com>
2022-10-10 10:56:03 -0400

    ok

--- src/app/successAlert/successAlert.component.ts
+++ src/app/successAlert/successAlert.component.ts
@@ -7,9 +7,24 @@ import { Component } from '@angular/core'
 })
 
 export class SuccessAlertComponent{
+    username: string ="";
+    disableButton = true;
 
     Constructor(){
 
     }
-
+    UserEntry(event : Event){
+        this.username = (<HTMLInputElement>event.target).value;
+        if(this.username === ""){
+            this.disableButton = true;
+        }
+        else this.disableButton = false;
+    }
+    EnableButton(){
+        return this.disableButton;
+    }
+    
+    ResetUsername(){
+        
+    }
 }
